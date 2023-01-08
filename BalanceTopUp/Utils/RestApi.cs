1947ce90b5702381079a3597455697ff1b8cff90
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2023-01-08 14:25:15 +0600

    Add helper RestApi class for use PostAsync to services

---
+++ BalanceTopUp/Utils/RestApi.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceTopUp.Utils
+{
+    public static class RestApi
+    {
+        private const string MediaType = "application/json";
+
+        public static async Task<string> PostAsync(string url, string data, HttpClient httpClient)
+        {
+            var encodingData = new StringContent(data, Encoding.UTF8, MediaType);
+            using (httpClient)
+            {
+                var response = await httpClient.PostAsync(url, encodingData);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
