93f6a785e0896f202ea4c7bf14469e0e3c47a455
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2023-01-08 22:38:44 +0600

    add nlog

--- BalanceTopUp/Program.cs
+++ BalanceTopUp/Program.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace BalanceTopUp
 {
@@ -13,11 +9,24 @@ namespace BalanceTopUp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder
+                .ConfigureNLog("nlog.config")
+                .GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("Start");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                logger.Error($"Ошибка: {e.Message}");
+                throw;
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .UseNLog();
     }
 }
