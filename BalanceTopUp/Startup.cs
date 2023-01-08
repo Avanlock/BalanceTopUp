57988c248fa74880027805ca643418116d1ca0f6
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2023-01-08 14:24:30 +0600

    Register all needed dependencies

--- BalanceTopUp/Startup.cs
+++ BalanceTopUp/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BalanceTopUp.Infrastructures.Databases;
+using BalanceTopUp.Repositories;
+using BalanceTopUp.Repositories.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +30,10 @@ namespace BalanceTopUp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("DbConnectionString");
+            services.AddDbContext<PaymentDbContext>(option => option.UseNpgsql(connection));
+            services.AddScoped<IPaymentRepository, PaymentRepository>();
+            services.AddHttpClient();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
