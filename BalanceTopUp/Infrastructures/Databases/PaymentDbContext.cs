739887d8cc804cbc0a5f6c06862df26f849cb8b3
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2023-01-08 14:32:30 +0600

    Add DbContext

---
+++ BalanceTopUp/Infrastructures/Databases/PaymentDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BalanceTopUp.Models.BdModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BalanceTopUp.Infrastructures.Databases
+{
+    public class PaymentDbContext : DbContext
+    {
+        public DbSet<Payment> Payments { get; set; }
+
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
