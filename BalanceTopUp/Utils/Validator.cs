a0640d775bbf4c4b439ade17a74e262154e36a10
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2023-01-08 14:23:48 +0600

    Add validator for phone number

---
+++ BalanceTopUp/Utils/Validator.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BalanceTopUp.Utils
+{
+    public static class Validator
+    {
+        public static bool CheckNumber(string number)
+        {
+            return Regex.Match(number, @"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$").Success;
+        }
+    }
+}
