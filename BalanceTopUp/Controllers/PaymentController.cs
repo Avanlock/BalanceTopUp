16343b04aa85d1d10a5f5e97676009c76527fc0b
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2023-01-08 14:31:42 +0600

    Add PaymentController with CratePayment method

---
+++ BalanceTopUp/Controllers/PaymentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BalanceTopUp.Models.BdModels;
+using BalanceTopUp.Models.RequestModels;
+using BalanceTopUp.Repositories.Interfaces;
+using BalanceTopUp.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BalanceTopUp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentRepository _paymentRepository;
+        private readonly HttpClient _httpClient;
+
+        public PaymentController(IPaymentRepository paymentRepository, HttpClient httpClient)
+        {
+            _paymentRepository = paymentRepository;
+            _httpClient = httpClient;
+        }
+        
+        [HttpPost]
+        [Route("CratePayment")]
+        public async Task<ActionResult> CratePaymentAsync(PaymentRequest paymentRequest)
+        {
+            try
+            {
+                if (Validator.CheckNumber(paymentRequest.Number))
+                {
+                    var phoneOperatorResult = new OperatorsHelper(_httpClient).DefineOperator(paymentRequest);
+
+                    if (phoneOperatorResult.Result == "success")
+                    {
+                        var payment = new Payment()
+                        {
+                            Number = paymentRequest.Number,
+                            Amount = paymentRequest.Amount
+                        };
+                        await _paymentRepository.Create(payment);
+                        return Ok(new PaymentResponse() { Message = "Success" });
+                    }
+                    return BadRequest(new PaymentResponse() { Message = $"Fail with message: {phoneOperatorResult.Result}" });
+                }
+                return BadRequest(new PaymentResponse() { Message = $"Not valid number" });
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new PaymentResponse() { Message = $"Fail with error: {ex.Message}" }); 
+            }
+        }
+    }
+}
