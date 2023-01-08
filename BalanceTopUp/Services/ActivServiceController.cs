1ee3b810df82446729ffec388c4781a8936e812a
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2023-01-08 14:33:23 +0600

    Add fake activ service

---
+++ BalanceTopUp/Services/ActivServiceController.cs
@@ -0,0 +1,17 @@
+﻿using BalanceTopUp.Models.BdModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BalanceTopUp.Services
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivServiceController : Controller
+    {
+        [HttpPost]
+        [Route("TopUpBalance")]
+        public string TopUpBalance(Payment payment)
+        {
+            return "success";
+        }
+    }
+}
