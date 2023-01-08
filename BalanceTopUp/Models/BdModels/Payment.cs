9f6711bc58809b2486c64be89820b1dce1377833
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2023-01-08 14:31:59 +0600

    Add Payment model

---
+++ BalanceTopUp/Models/BdModels/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BalanceTopUp.Models.BdModels
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public int Amount { get; set; }
+        public DateTime ReplenishmentDate { get; set; } = DateTime.Now;
+    }
+}
